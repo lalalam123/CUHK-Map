ce2d8dba4b5828d9ac45ea8a2a3692134623f611
lalalam123 <90650499+lalalam123@users.noreply.github.com>
2024-04-23 12:48:41 +0800

    add loading pages

---
+++ src/app/[locale]/facility/loading.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { MapLoadingWidget } from "@/components/loadingWidget";
+
+export default function page() {
+  return (
+    <div>
+      <MapLoadingWidget />
+    </div>
+  );
+}
